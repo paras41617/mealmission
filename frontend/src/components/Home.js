7b7b41a5acf283bdd87cc9bd5f1fd169e17ad57b
PARAS GUPTA <paras41617@gmail.com>
2023-03-20 10:45:19 +0530

    signup_page

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styles/home.css'
+
+class Home extends React.Component {
+
+    // redirect_signup(){
+    //     window.location.href = '/signup'
+    // }
+
+	render() {
+		return (
+            <div>
+                {/* <button onClick={this.redirect_signup}>Signup</button>  */}
+            </div>
+		);
+	}
+}
+
+export default Home;
---
+++ frontend/src/components/Signup.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Signup extends React.Component {
+
+	render() {
+		return (
+            <div>
+                Hello Signup
+            </div>
+		);
+	}
+}
+
+export default Signup;
