d12495904e886168a77f7e2b742b64eb18f305d2
ShubhT <Shubhcodes1625@gmail.com>
2023-03-20 12:00:36 +0530

    signup_page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,19 +1,78 @@
-import React from 'react';
-import '../styles/home.css'
+import React, { Component } from 'react';
+import '../styles/home.css';
 
-class Home extends React.Component {
+class Home extends Component {
+  state = {
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  };
 
-    // redirect_signup(){
-    //     window.location.href = '/signup'
-    // }
+  handleInputChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
 
-	render() {
-		return (
-            <div>
-                {/* <button onClick={this.redirect_signup}>Signup</button>  */}
-            </div>
-		);
-	}
+  handleFormSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    const { username, email, password, confirmPassword } = this.state;
+
+    return (
+    
+      <div className="signup-page">
+        <h1 className="mealmission">MealMiSSion</h1>
+        <h1>Sign Up</h1>
+        <form onSubmit={this.handleFormSubmit}>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              name="username"
+              value={username}
+              onChange={this.handleInputChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={this.handleInputChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={this.handleInputChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="confirmPassword">Confirm Password</label>
+            <input
+              type="password"
+              name="confirmPassword"
+              value={confirmPassword}
+              onChange={this.handleInputChange}
+              required
+            />
+          </div>
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Home;
